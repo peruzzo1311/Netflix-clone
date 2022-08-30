a705f2b5b6ea427dc48ce4ef2d31fd72586bebfa
Gustavo Peruzzo <gustavoperuzzo1311@hotmail.com>
2022-08-30 17:55:38 -0300

    30/08/2022

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -4,16 +4,20 @@ import './List.css'
 
 export default ({ title, items }) => {
   return (
-    <div>
+    <div className='movie'>
       <h2>{title}</h2>
       <div className='movieRow'>
-        {items.results.length > 0 &&
-          items.results.map((item, key) => (
-            <img
-              src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
-              alt={item.original_title}
-            />
-          ))}
+        <div className='movieRow-list'>
+          {items.results.length > 0 &&
+            items.results.map((item, key) => (
+              <div className='movieRow-item'>
+                <img
+                  src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
+                  alt={item.original_title}
+                />
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   )
