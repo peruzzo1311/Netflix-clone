4c688f518b36411cc5dd7afb3ce16e8766967507
Gustavo Peruzzo <gustavoperuzzo1311@hotmail.com>
2022-09-20 17:54:32 -0300

    20/09/22

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,5 +2,24 @@ import React from 'react'
 import './Header.css'
 
 export default () => {
-  return <div>...</div>
+  return (
+    <header>
+      <div className='header-logo'>
+        <a href='/'>
+          <img
+            src='https://images.ctfassets.net/4cd45et68cgf/7LrExJ6PAj6MSIPkDyCO86/542b1dfabbf3959908f69be546879952/Netflix-Brand-Logo.png?w=684&h=456'
+            alt='logo'
+          />
+        </a>
+      </div>
+      <div className='header-user'>
+        <a href='/'>
+          <img
+            src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
+            alt='user'
+          />
+        </a>
+      </div>
+    </header>
+  )
 }
